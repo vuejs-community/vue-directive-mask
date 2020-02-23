0694810ad1ce99dd1f514f97d4c27ae62f431bb4
Eduard Nikolenko <mail@eduardnikolenko.com>
2020-02-23 21:09:11 +0300

    Export TS declarations

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
-import rollupPluginJson from 'rollup-plugin-json';
-import rollupPluginTypeScript from 'rollup-plugin-typescript';
+import rollupPluginJson from '@rollup/plugin-json';
+import rollupPluginTypeScript from '@wessberg/rollup-plugin-ts';
 
 export default {
-  experimentalCodeSplitting: true,
   input: './src/main.ts',
   output: {
     exports: 'named'
